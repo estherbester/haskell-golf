1f49b9f695b52fa2fb7cc6120c202a946fa61e07
Esther N <esther.nam@gmail.com>
2014-08-16 21:59:24 -0700

    alt caps

--- golf.hs
+++ golf.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 -- palindrome
 
 palin a = a == reverse a
@@ -9,3 +11,6 @@ count_vowels w = sum [ 1 | x <- w, x `elem` "AEIOUaeiou"]
 -- returns true iff there are an odd number of 1s in the list
 parity w = if odd (sum[1 | x <- w, x=='1']) then True else False
 
+-- alt_caps
+r = cycle [toUpper, toLower]
+alt_caps w = [(fst x) (snd x) | x <- zip r w] 
