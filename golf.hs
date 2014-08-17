2fd5f9813bcd93b0f2cdaf206c9bfae449010c67
Esther N <esther.nam@gmail.com>
2014-08-16 21:59:07 -0700

    parity

--- golf.hs
+++ golf.hs
@@ -4,3 +4,8 @@ palin a = a == reverse a
 
 -- count vowels
 count_vowels w = sum [ 1 | x <- w, x `elem` "AEIOUaeiou"]
+
+-- parity
+-- returns true iff there are an odd number of 1s in the list
+parity w = if odd (sum[1 | x <- w, x=='1']) then True else False
+
