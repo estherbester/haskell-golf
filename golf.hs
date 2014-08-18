42edf9342cd3876f37827b31ef0a7e5f105182a3
Esther N <esther.nam@gmail.com>
2014-08-17 18:55:19 -0700

    last three

--- golf.hs
+++ golf.hs
@@ -1,7 +1,6 @@
 import Data.Char
 
 -- palindrome
-
 palin a = a == reverse a
 
 -- count vowels
@@ -14,3 +13,23 @@ parity w = if odd (sum[1 | x <- w, x=='1']) then True else False
 -- alt_caps
 r = cycle [toUpper, toLower]
 alt_caps w = [(fst x) (snd x) | x <- zip r w] 
+
+-- is_triangle
+-- i can't do this right now
+is_triangle a b c
+	| a + b <= c = False
+	| a + c <= b = False 
+	| b + c <= a = False
+	| otherwise = True
+
+-- next color
+c = cycle ["GREEN", "YELLOW", "RED"]
+next_color w = dropWhile (/= w) c !! 1 
+
+-- fib
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2) 
+    
+
